2d9118cc44b828c85c4d729d3e5d15241ca99ea1
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2020-06-25 16:48:38 +0530

    Update store.js
    added changes to set store

--- clientpanel/src/store.js
+++ clientpanel/src/store.js
@@ -4,4 +4,44 @@ import 'firebase/firestore';
 import { reactReduxFirebase,firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 
-const fbConfig = {}
+const fireBaseConfig = {
+    apiKey: "AIzaSyDiz61a1JXFSyXAXxr_ORTJ6khqsJVdPO4",
+    authDomain: "clientpanel-930fd.firebaseapp.com",
+    databaseURL: "https://clientpanel-930fd.firebaseio.com",
+    projectId: "clientpanel-930fd",
+    storageBucket: "clientpanel-930fd.appspot.com",
+    messagingSenderId: "189289296669",
+    appId: "1:189289296669:web:d54f92f01308aacdba67f4",
+    measurementId: "G-7TLVQLYML3"
+  };
+
+  const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true
+  };
+
+  firebase.initializeApp(fireBaseConfig);
+  const fireStore = firebase.firestore();
+
+  const createStoreWithFirebase = compose(
+      reactReduxFirebase(firebase, rrfConfig),
+      reduxFirestore(firebase)
+  )(createStore);
+
+  const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer
+  });
+
+  const initialState = {};
+
+  const store = createStoreWithFirebase(
+      rootReducer, 
+      initialState, 
+      compose(
+      reactReduxFirebase(firebase),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
