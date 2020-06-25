5e987d5dd58b51a8687750f739fb9d076365f2ce
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2020-06-25 16:49:10 +0530

    Update App.js

--- clientpanel/src/App.js
+++ clientpanel/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import {Provider} from 'react-redux';
+import store from './store'
+
 import AppNavbar from './components/layouts/AppNavbar';
 import Dashboard from './components/layouts/Dashboard';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <AppNavbar></AppNavbar>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Dashboard}></Route>
-          </Switch>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+         <AppNavbar></AppNavbar>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard}></Route>
+            </Switch>
+          </div>
         </div>
-    </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
